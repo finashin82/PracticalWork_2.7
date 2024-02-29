6749124aea3a1dae777ec0e332ccc4666f9b8933
Роман <finashin82@mail.ru>
2024-02-29 16:25:05 +0900

    Готовое домашнее задание

--- PracticalWork_2.7/Program.cs
+++ PracticalWork_2.7/Program.cs
@@ -10,6 +10,34 @@ namespace PracticalWork_2._7
     {
         static void Main(string[] args)
         {
+            // Задание 1
+            string fullName = "Иванов Иван Иванович";
+            string email = "ivanov@mail.ru";
+            float programmingScores = 3.5f;
+            float mathScores = 4.5f;
+            float physicsScores = 4.3f;
+            int age = 18;
+
+            Console.WriteLine("Задание 1");
+            Console.WriteLine();
+            Console.WriteLine($"Имя: {fullName} \nВозраст: {age} \n" +
+                $"Почта: {email} \nБаллы по программированию: {programmingScores} \nБаллы по математике: {mathScores} \n" +
+                $"Баллы по физике: {physicsScores}");
+
+            Console.WriteLine();
+            Console.WriteLine();      
+
+            // Задание 2
+            float amountOfPoints;
+            float averageScore;
+
+            amountOfPoints = programmingScores + mathScores + physicsScores;
+            averageScore = amountOfPoints / mathScores;
+
+            Console.WriteLine("Задание 2");
+            Console.WriteLine();
+            Console.WriteLine($"Сумма баллов: {amountOfPoints} \nСредний балл: {averageScore:#.##}");
+            Console.ReadLine();
         }
     }
 }
